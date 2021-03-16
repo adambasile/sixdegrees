8953b0a71dbc9218213168cec5415b88216be72e
Adam Basile <adbas97@gmail.com>
2021-03-16 22:45:37 +1100

    Migrate to FirstCycling (#1)
    * Attempt to shift to FirstCycling with scrpaer which should work but doesn't
    
    * Firstcycling successfully scraped
    
    * Ported to FirstCycling
    
    * Remove some bogus entries

--- scraping_and_analysis/scrapers.py
+++ scraping_and_analysis/scrapers.py
@@ -1,72 +1,102 @@
 from bs4 import BeautifulSoup
 import requests
-
+from retrying import retry
 import pandas as pd
+import re
 
-ranking_page = "https://www.procyclingstats.com/rankings.php?date={}-12-31&nation=&age=&zage=&page=smallerorequal&team=&offset={}&filter=Filter&p={}e&s=season-individual"
+ranking_page = "https://firstcycling.com/ranking.php?k=fc&rank={}&y={}&page={}"
 
 
 def scrape_top(args):
-    year, offset, mens = args
-    gender = "m" if mens else "w"
-    with requests.get(ranking_page.format(year, offset, gender)) as fp:
+    year, page, mens = args
+    gender = "el" if mens else "wel"
+    with requests.get(ranking_page.format(gender, year, page)) as fp:
         soup = BeautifulSoup(fp.text, "html.parser")
-    top_riders = [a["href"] for a in soup.find("table", **{"class": "basic"}).find_all("a")[::3]]
-    return top_riders
+
+    return [int(tr.find("a")["href"].split("=")[1]) for tr in soup.find("tbody").find_all("tr")]
 
 
+@retry(stop_max_attempt_number=3)
 def scrape_rider(rider):
     try:
-        with requests.get(f"https://www.procyclingstats.com/{rider}") as fp:
+        with requests.get(f"https://firstcycling.com/rider.php?r={rider}&teams=1") as fp:
             soup = BeautifulSoup(fp.text, "html.parser")
-
-        with requests.get(f"https://www.procyclingstats.com/{rider}/statistics/season-statistics") as fp:
-            pointsoup = BeautifulSoup(fp.text, "html.parser")
-
-        teams = pd.Series(
-            {
-                int(teamyear.find("div", **{"class": "season"}).text): teamyear.find("a")["href"]
-                for teamyear in soup.find("ul", **{"class": "rdr-teams"}).find_all("li")
-                if teamyear.find("div", **{"class": "season"}).text
-            }
-        )
-        teams = teams[teams.index < 2022]
-
-        points = pd.read_html(str(pointsoup.find("table")))[0].dropna().astype(int).set_index("Season")[["Points"]]
-
-        out = points.reindex(teams.index).fillna(0)
-        out["Teams"] = teams
-        out["Rider"] = rider
-        return list(out[["Rider", "Teams", "Points"]].itertuples(index=False, name=None))
-
+        tables = soup.find_all("table", **{"class": "tablesorter"})
+        if len(tables) == 1:
+            return []
+        return [(rider, int(a["href"].split("=")[1])) for a in tables[1].find_all("a")]
     except Exception as e:
         import sys
 
         raise type(e)(f"Problem with '{rider}'").with_traceback(sys.exc_info()[2])
 
 
+@retry(stop_max_attempt_number=3)
 def scrape_team(team):
     try:
-        with requests.get(f"https://www.procyclingstats.com/{team}") as fp:
+        with requests.get(f"https://firstcycling.com/team.php?l={team}&riders=2") as fp:
             soup = BeautifulSoup(fp.text, "html.parser")
-        return [a["href"] for a in soup.find("ul", style=" ", **{"class": ("list", "pad2")}).find_all("a")]
+        return [
+            int(a["href"].split("=")[1])
+            for a in soup.find_all("table", **{"class": "tablesorter"})[0].find_all("a")[::2]
+        ]
+    except Exception as e:
+        import sys
+
+        raise type(e)(f"Problem with '{team}'").with_traceback(sys.exc_info()[2])
+
+
+@retry(stop_max_attempt_number=3)
+def get_pretty_rider(rider):
+    try:
+        with requests.get(f"https://firstcycling.com/rider.php?r={rider}") as fp:
+            soup = BeautifulSoup(fp.text, "html.parser")
+        return {"name": soup.find("h1").text, "url": rider, "year": 0}
+    except Exception as e:
+        import sys
+
+        raise type(e)(f"Problem with '{rider}'").with_traceback(sys.exc_info()[2])
+
+        
+year_pat = re.compile(r"r=\d+&y=(\d\d\d\d)")
+
+
+@retry(stop_max_attempt_number=3)
+def get_pretty_team(team):
+    try:
+        with requests.get(f"https://firstcycling.com/team.php?l={team}") as fp:
+            soup = BeautifulSoup(fp.text, "html.parser")
+         
+        year_match = re.search(year_pat, fp.text)
+        if year_match:
+            year = int(year_match.group(1))
+        else:
+            try:
+                year = int(soup.find("option", selected=True).text.split()[0])
+            except:
+                year = 1
+        return {
+            "name": soup.find("h1").text,
+            "url": -team,
+            "year": year,
+        }
     except Exception as e:
         import sys
 
         raise type(e)(f"Problem with '{team}'").with_traceback(sys.exc_info()[2])
         
         
-def get_pretty_rider(rider):
-    with requests.get(f"https://www.procyclingstats.com/{rider}") as fp:
-        soup = BeautifulSoup(fp.text, "html.parser")
-    return {"name": " ".join(soup.title.text.strip().split()), "url": rider.split("/", 1)[1], "year": 0}
+@retry(stop_max_attempt_number=3)
+def team_year_from_rider(args):
+    rider, team = args
+    try:
+        with requests.get(f"https://firstcycling.com/rider.php?r={rider}&teams=1") as fp:
+            soup = BeautifulSoup(fp.text, "html.parser")
 
+        team_row, = [tr for tr in soup.find("div", **{"id":"wrapper"}).find_all("tr") if f"team.php?l={team}" in str(tr)]
+        return {team: int(team_row.find("td").text)}
+    except Exception as e:
+        import sys
 
-def get_pretty_team(team):
-    with requests.get(f"https://www.procyclingstats.com/{team}") as fp:
-        soup = BeautifulSoup(fp.text, "html.parser")
-    return {
-        "name": soup.find("h1").text,
-        "url": team.split("/", 1)[1],
-        "year": int(soup.find("span", **{"class": ["red"]}).text),
-    }
+        raise type(e)(f"Problem with '{rider}', '{team}'").with_traceback(sys.exc_info()[2])
+       
